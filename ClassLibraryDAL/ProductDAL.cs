4671832d10cf7f87d0d7ea10eee18b8c70e61305
Hashir razzaq <70018752+Hashirrazzaq256@users.noreply.github.com>
2023-01-01 15:25:22 +0500

    Add files via upload

---
+++ ClassLibraryDAL/ProductDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClassLibraryDAL
+{
+    public class ProductDAL
+    {
+        public static int SaveProduct()
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            string command = "insert into product(name)values('fanta')";
+            SqlCommand cmd = new SqlCommand(command, con);
+            int i= cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+
+        }
+
+        public static void DeleteProduct()
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            string command = "DELETE FROM product WHERE ID=(SELECT MAX(id) FROM product)";
+            SqlCommand cmd = new SqlCommand(command, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+    }
+}
